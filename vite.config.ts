405ac8bd60ba26cb5b7690b6f4ceb9b59de68e60
trrainss <egorlevsha2007@gmail.com>
2026-06-26 13:15:35 +0300

    Исправлен 404 на Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
-import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': '/src',
     },
-  },
-  server: {
-    port: 5173,
   },
 });
